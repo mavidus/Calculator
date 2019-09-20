1d167578e3a6cee2cff90aaae42a33b09b9dd326
mavidus <mavdus2012@gmail.com>
2019-09-20 12:44:08 -0600

    add comment to method

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,16 +1,18 @@
 public class Calculator {
+    //add comment here
 
-    public static double add(double num1, double num2){
+    public static double add(double num1, double num2) {
 
-        return num1+num2;
+        return num1 + num2;
     }
 
-    public static double subtract(double num1, double num2){
-        double result =num1 -num2;
+    public static double subtract(double num1, double num2) {
+        double result = num1 - num2;
         return result;
     }
-    
-    public static double multiply(double num1, double num2){
-        
-        return num1*num2;
+
+    public static double multiply(double num1, double num2) {
+
+        return num1 * num2;
+    }
 }
